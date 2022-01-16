9acde588481e7925407a9fd0fd834fc36586edb7
Murtuza Khan <mohdmurtuzakhan@gmail.com>
2022-01-16 18:11:18 +0530

    Addupto function

---
+++ bigo.js
@@ -0,0 +1,14 @@
+
+function addUpTo(n){
+    let total = 0;
+    for (let i = 0; i < n; i++) {
+        total+=i;
+    }
+    return total;
+}
+
+let t1 = performance.now();
+addUpTo(10000000000);
+// addUpTo(10);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2-t1)/1000} seconds.`);
